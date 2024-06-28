c7dce18de8e15811992008b345b52ee75f1dc724
Nikolay <kondalovnikolay@gmail.com>
2024-06-28 10:22:22 +0500

    Добавьте файлы проекта.

---
+++ HM_9_WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using HM_9_WPF.WorkWindow;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HM_9_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void buttonMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void buttonCloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonStartClick(object sender, RoutedEventArgs e)
+        {
+            MyWorkWindow window = new MyWorkWindow();
+
+            window.Show();
+
+            this.Close();
+        }
+    }
+}
---
+++ HM_9_WPF/WorkWindow/MyWorkWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HM_9_WPF.WorkWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MyWorkWindow.xaml
+    /// </summary>
+    public partial class MyWorkWindow : Window
+    {
+        public MyWorkWindow()
+        {
+            InitializeComponent();
+            txtText.PreviewTextInput += InputTextBoxSplit_PreviewTextInput;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void buttonMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void buttonCloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonBackClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void InputTextBoxSplit_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Проверка на ввод чисел
+            if (IsNumber(e.Text))
+            {
+                // запрет на ввод чисел
+                e.Handled = true;
+
+                TextBlock errorMessageBlock = new TextBlock
+                {
+                    Text = "Ввод чисел запрещен!",
+                    Foreground = new SolidColorBrush(Colors.Red)
+                };
+
+                if (IsInputTextBoxEmpty(txtText))
+                {
+                    wordsListBox.Items.Clear();
+                }
+
+                wordsListBox.Items.Add(errorMessageBlock);
+            }
+        }
+
+        private bool IsNumber(string text)
+        {
+            foreach (char c in text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void OnSplitWordsClick(object sender, RoutedEventArgs e)
+        {
+            if (IsInputTextBoxEmpty(txtText))
+            {
+                DisplayErrorMassege("Ввод не может быть пустым!!! ");
+                return;
+            }
+
+            wordsListBox.Items.Clear();
+
+            //для TextBlock
+            //wordsListBox.Inlines.Clear();
+
+            string inputText = txtText.Text;
+
+            string[] words = inputText.Split(' ');
+
+            foreach (string word in words)
+            {
+                ListBoxItem item = new ListBoxItem
+                {
+                    Content = word,
+                    Foreground = new SolidColorBrush(Colors.Red)
+                };
+
+                wordsListBox.Items.Add(item);
+
+                //для TextBlock
+                //wordsListBox.Inlines.Add(item);
+            }
+        }
+
+
+        public void OnReverseWordsClick(object sender, RoutedEventArgs e)
+        {
+            if (IsInputTextBoxEmpty(txtText))
+            {
+                DisplayErrorMassege("Ввод не может быть пустым!!! ");
+                return;
+
+            }
+
+        }
+        public bool IsInputTextBoxEmpty(TextBox textBox)
+        {
+            return string.IsNullOrEmpty(textBox.Text);
+        }
+
+        private void DisplayErrorMassege(string errorMessage)
+        {
+            ListBoxItem errorItem = new ListBoxItem
+            {
+                Content = new TextBlock
+
+                {
+                    Text = errorMessage,
+                    Foreground = new SolidColorBrush(Colors.Red)
+                }
+
+            };
+
+            if (IsInputTextBoxEmpty(txtText))
+            {
+                wordsListBox.Items.Clear();
+            }
+
+            wordsListBox.Items.Add(errorItem);
+
+            //для TextBlock
+            //wordsListBox.Inlines.Add(errorItem);
+        }
+    }
+}
